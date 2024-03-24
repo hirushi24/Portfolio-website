6744629c5a421cf0283d4dbd1017a9aabc5d3f6d
hirushi24 <hirushi.20220615@iit.ac.lk>
2024-03-24 20:18:49 +0530

    edit page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
-import AchievementsSection from "./components/AchievementsSection";
 
 export default function Home() {
   return (
@@ -15,7 +14,6 @@ export default function Home() {
         <HeroSection />
         <AchievementsSection01/>
         <AboutSection />
-        <AchievementsSection />
         <ProjectsSection />
         <EmailSection />
       </div>
